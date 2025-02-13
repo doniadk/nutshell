abfa8c3d0deeffb1f1d508a9d9f92b1c8f9a4256
doniadk <khairallahdonia05@gmail.com>
2025-02-13 23:55:23 +0100

    added NotFoundPage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import MapPage from "./pages/MapPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import NotFoundPage from "./pages/NotFoundPAge";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MapPage />} />
+        <Route path="*" element={<NotFoundPage />} />
         {/*<Route path="/country/:id" element={<CountryPage />} />*/}
       </Routes>
     </Router>
