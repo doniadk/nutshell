db1d7567dd67998e11d032d11267133120f65bb7
doniadk <khairallahdonia05@gmail.com>
2025-02-28 21:28:35 +0100

    configuring package files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Frontend runs on port 3000
-  },
 });
